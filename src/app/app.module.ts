e12a409f4c12d1c200b85d898f1ec4f36347cdfe
Tshepho <tshepomoshabane3@gmail.com>
2022-07-17 19:51:42 +0200

    feat-about-us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,38 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routing';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"; // this is needed!
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app.routing";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { LandingComponent } from './pages/landing/landing.component';
-import { AOS } from 'aos';
-import { AboutUsComponent } from './pages/about-us/about-us.component';
-import { OurServicesComponent } from './pages/our-services/our-services.component';
-import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { LandingComponent } from "./pages/landing/landing.component";
+
+import { AboutUsComponent } from "./pages/about-us/about-us.component";
+import { OurServicesComponent } from "./pages/our-services/our-services.component";
+import { ContactUsComponent } from "./pages/contact-us/contact-us.component";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavbarComponent,
-        LandingComponent,
-        AboutUsComponent,
-        OurServicesComponent,
-        ContactUsComponent
-    ],
-    imports: [
-        BrowserAnimationsModule,
-        NgbModule,
-        FormsModule,
-        RouterModule,
-        AppRoutingModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LandingComponent,
+    AboutUsComponent,
+    OurServicesComponent,
+    ContactUsComponent,
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    NgbModule,
+    FormsModule,
+    RouterModule,
+    BrowserModule /* or CommonModule */,
+    ReactiveFormsModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,18 +1,136 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact-us',
-  templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  selector: "app-contact-us",
+  templateUrl: "./contact-us.component.html",
+  styleUrls: ["./contact-us.component.css"],
 })
 export class ContactUsComponent implements OnInit {
   zoom: number = 14;
   lat: number = 44.445248;
   lng: number = 26.099672;
-  styles: any[] = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}];
-  constructor() { }
+  styles: any[] = [
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#e9e9e9" }, { lightness: 17 }],
+    },
+    {
+      featureType: "landscape",
+      elementType: "geometry",
+      stylers: [{ color: "#f5f5f5" }, { lightness: 20 }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.fill",
+      stylers: [{ color: "#ffffff" }, { lightness: 17 }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }],
+    },
+    {
+      featureType: "road.arterial",
+      elementType: "geometry",
+      stylers: [{ color: "#ffffff" }, { lightness: 18 }],
+    },
+    {
+      featureType: "road.local",
+      elementType: "geometry",
+      stylers: [{ color: "#ffffff" }, { lightness: 16 }],
+    },
+    {
+      featureType: "poi",
+      elementType: "geometry",
+      stylers: [{ color: "#f5f5f5" }, { lightness: 21 }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry",
+      stylers: [{ color: "#dedede" }, { lightness: 21 }],
+    },
+    {
+      elementType: "labels.text.stroke",
+      stylers: [{ visibility: "on" }, { color: "#ffffff" }, { lightness: 16 }],
+    },
+    {
+      elementType: "labels.text.fill",
+      stylers: [{ saturation: 36 }, { color: "#333333" }, { lightness: 40 }],
+    },
+    { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
+    {
+      featureType: "transit",
+      elementType: "geometry",
+      stylers: [{ color: "#f2f2f2" }, { lightness: 19 }],
+    },
+    {
+      featureType: "administrative",
+      elementType: "geometry.fill",
+      stylers: [{ color: "#fefefe" }, { lightness: 20 }],
+    },
+    {
+      featureType: "administrative",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#fefefe" }, { lightness: 17 }, { weight: 1.2 }],
+    },
+  ];
+  emailForm: FormGroup;
+
+  validationMessages = {
+    required: [{ type: "required", message: "This field is required." }],
+    email: [
+      { type: "required", message: "This field is required." },
+      { type: "email", message: "Please enter a valid email." },
+    ],
+  };
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.emailForm = this.fb.group({
+      name: [null, [Validators.required]],
+      surname: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      message: [null, [Validators.required]],
+    });
   }
 
+  sendEmail() {
+    this.emailForm.markAllAsTouched();
+    this.emailForm.updateValueAndValidity();
+
+    if (this.emailForm.valid) {
+      const emailTo = "auctuminvestments@outlook.com";
+      const emailCC = "";
+      const emailSub = "Auctum website";
+      const emailBody =
+        "My name is " +
+        this.emailForm.value.name +
+        " " +
+        this.emailForm.value.surname +
+        ", " +
+        this.emailForm.value.message;
+      window.open(
+        `mailto:${emailTo}?cc=${emailCC}&subject=${emailSub}&body=${emailBody}`,
+        "_self"
+      );
+    }
+  }
+  isInvalid(form: FormGroup, controlName: string) {
+    return form.get(controlName).invalid && form.get(controlName).touched;
+  }
+
+  isValid(form: FormGroup, controlName: string) {
+    return form.get(controlName).valid && form.get(controlName).touched;
+  }
+
+  hasError(form: FormGroup, controlName: string, validationType: string) {
+    // eslint-disable-next-line max-len
+    return (
+      form.get(controlName).hasError(validationType) &&
+      (form.get(controlName).dirty || form.get(controlName).touched)
+    );
+  }
 }
